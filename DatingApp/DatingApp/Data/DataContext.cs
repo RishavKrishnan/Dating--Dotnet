a9fa4e4cc540484cb566f0553b07d54b428caf15
RishavKrishnan <rishavkrishnan2016@gmail.com>
2025-02-21 11:26:49 +0530

    Initial commit - Upload DatingApp project

---
+++ DatingApp/DatingApp/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContex(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> User { get; set;} //we'll get 
+    }
+}
---
+++ DatingApp/DatingApp/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace DatingApp.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; } // since it's called Id, this property will be the primary key by default
+        public required string UserName { get; set; } 
+
+    }
+}
